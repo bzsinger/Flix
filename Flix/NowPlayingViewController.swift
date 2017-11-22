11d945628ed5eec71ef590b9fb4575e9025dbfa6
bzsinger <benjamin.z.singer@gmail.com>
2017-11-22 14:34:55 -0600

    Get movies from The Movie DB
    Hide API key in ignored file APIKey.swift

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Benny Singer on 11/22/17.
+//  Copyright © 2017 bzsinger. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=" + APIKey)!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        // Asynchronous
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when network request returns
+            
+            // checks if error nil, if it is, ignore if
+            // otherwise, put error into 'error'
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                // because !, will crash if exception thrown
+                // cast as dictionary, String: Any
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+            }
+        }
+        
+        // to actually start task
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
