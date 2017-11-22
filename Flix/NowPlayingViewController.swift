287a9ec57c1ced4fb7bf6e93db552b72988a0941
bzsinger <benjamin.z.singer@gmail.com>
2017-11-22 14:56:10 -0600

    Add table view
    Add table view, custom cell to Storyboard. Set ViewController to be TableViewDataSource.

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=" + APIKey)!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -30,12 +35,26 @@ class NowPlayingViewController: UIViewController {
                 // because !, will crash if exception thrown
                 // cast as dictionary, String: Any
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
             }
         }
         
         // to actually start task
         task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,7 +62,6 @@ class NowPlayingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
